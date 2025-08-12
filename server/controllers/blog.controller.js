825fed24aca85fde3725f16db9c0b21e99ccd521
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2025-08-13 01:16:45 +0530

    added APIs of add blog, delete blog, get blog by id, get all blogs, add comments, get comments

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -1,8 +1,9 @@
 import fs from "fs";
 import imagekit from "../config/imageKit.js";
 import Blog from "../models/blog.model.js";
+import Comment from "../models/comment.model.js";
 
-export const addBLog = async (req, res) => {
+const addBlog = async (req, res) => {
   try {
     const { title, subTitle, description, category, isPublished } = JSON.parse(
       req.body.blog
@@ -10,7 +11,13 @@ export const addBLog = async (req, res) => {
     const imageFile = req.file;
 
     // check all fields persent
-    if (!title || !subTitle || !description || !category || !isPublished) {
+    if (
+      !title ||
+      !subTitle ||
+      !description ||
+      !category ||
+      !isPublished === undefined
+    ) {
       return res
         .status(400)
         .json({ success: false, message: "Missing required fields" });
@@ -36,7 +43,7 @@ export const addBLog = async (req, res) => {
     });
     const image = optimizedImageUrl;
 
-    await Blog.create({
+    const newBlog = await Blog.create({
       title,
       subTitle,
       description,
@@ -45,8 +52,113 @@ export const addBLog = async (req, res) => {
       isPublished,
     });
 
-    res.status(201).json({ success: true, message: "Blog added Successfully" });
+    res.status(201).json({
+      success: true,
+      message: "Blog added Successfully",
+      blog: newBlog,
+    });
   } catch (error) {
-    res.status(400).json({ success: false, message: error.message });
+    res.status(500).json({ success: false, message: error.message });
   }
+};
+
+const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+    res.status(200).json({ success: true, blogs });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const getBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Blog not found" });
+    }
+    res.status(200).json({ success: true, blog });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const deleteById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Blog.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Blog deleted Successfully " });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Blog ID is required" });
+    }
+
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Blog not found" });
+    }
+
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Blog status updated",
+      isPublished: blog.isPublished,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const addComment = async (req, res) => {
+  try {
+    const { blog, name, content } = req.body;
+    await Comment.create({ blog, name, content });
+    res
+      .status(201)
+      .json({ success: false, message: "Comment added for review" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const getBlogComment = async (req, res) => {
+  try {
+    const { blogId } = req.body;
+    const comments = Comment.find({ blog: blogId, isApproved: true }).sort({
+      createdAt: -1,
+    });
+    res.status(201).json({ success: true, comments });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export {
+  addBlog,
+  getAllBlogs,
+  getBlogById,
+  deleteById,
+  togglePublish,
+  addComment,
+  getBlogComment
 };
--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { addBLog } from "../controllers/blog.controller.js";
+import {
+  addBlog,
+  addComment,
+  deleteById,
+  getAllBlogs,
+  getBlogById,
+  getBlogComment,
+  togglePublish,
+} from "../controllers/blog.controller.js";
 import upload from "../middlewares/multer.js";
 import auth from "../middlewares/auth.middleware.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", upload.single("image"), auth, addBLog);
+blogRouter.post("/add", upload.single("image"), auth, addBlog);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.delete("/delete", auth, deleteById);
+blogRouter.post("/toggle-publish", auth, togglePublish);
+blogRouter.post("/addComment", addComment);
+blogRouter.get("/comments", getBlogComment);
 
 export default blogRouter;
