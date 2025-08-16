b37889a17d5d0da0be09782dd0884144e3eed9d5
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2025-08-16 18:15:58 +0530

    fixed Home and Newsletter case sensitivity

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Bloglist from "../components/BlogList";
-import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
+import Newsletter from "../components/NewsLetter";
 
 function Home() {
   return (
-    <>  
+    <>
       <Navbar />
       <Header />
       <Bloglist />
-      <NewsLetter />
+      <Newsletter />
       <Footer />
     </>
   );
