c0b93049c8c52f0250b802cf6f5598870b580d7c
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2025-08-16 17:24:43 +0530

    fixed Newsletter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Bloglist from "../components/BlogList";
-import NewsLetter from "../components/Newsletter";
+import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
 
 function Home() {
