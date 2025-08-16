5dbdd3714eca4112acb187d87d03a3959a24c3d9
Waseem-khan7 <waseemnasirkhan24@gmail.com>
2025-08-16 17:20:46 +0530

    fixl

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Bloglist from "../components/BlogList";
-import NewsLetter from "../components/NewsLetter";
+import NewsLetter from "../components/Newsletter";
 import Footer from "../components/Footer";
 
 function Home() {
